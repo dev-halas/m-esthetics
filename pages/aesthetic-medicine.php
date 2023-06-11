524c2922cbefb0bdcfe86f45c700e1a60e0d3ece
kamilogrodowczyk <kamil.web.development@gmail.com>
2023-06-11 17:39:01 +0200

    Create aesthetic medicine page

---
+++ pages/aesthetic-medicine.php
@@ -0,0 +1,229 @@
+<?php  
+
+    /* Template Name: Aesthetic Medicine Page */
+
+    get_header();
+
+?>
+    <main>
+        <section id="offer-page-big-image-one">
+            <div class="offer-page-image">
+                <img src="<?php echo THEME_URL; ?>_dev/img/beautiful-woman-face-aesthetic-medicine-page.jpg" alt="twarz kobiety">
+            </div>
+        </section>
+        <section>
+            <div class="container offer-page-container gridCols2">
+                <h2 class="offer-page-header"><span class="c-gold">toksyna</span></br>botulinowa</h2>
+                <div class="offer-page-text">
+                    <p>Toksyna botulinowa to neurotoksyna, która ma za zadanie blokowanie neuroprzekaźnika odpowiedzialnego za przekazywanie impulsów nerwowych do komórki mięśniowej.  
+                    Powoduje ona rozluźnienie mięśni.</p>
+                    <a class="offer-page-link" href="">Zobacz efekty</a>
+                </div>
+            </div>
+            <div class="offer-page-container-list gridCols3">
+                <div class="offer-page-list">
+                    <h3 class="h3">Wskazania</h3>
+                    <ul>
+                        <li>zmarszczki poprzeczne na czole,</li>
+                        <li>lwia zmarszczka,</li>
+                        <li>kurze łapki,</li>
+                        <li>zmarszczki królicze,</li>
+                        <li>broda marmurkowa,</li>
+                        <li>bruksizm,</li>
+                        <li>opadnięte kąciki ust,</li>
+                        <li>opadniete powieki.</li>
+                    </ul>
+                </div>
+                <div class="offer-page-list">
+                    <h3 class="h3">Przeciwwskazania</h3>
+                    <ul>
+                        <li>ciąża,</li>
+                        <li>karmienie piersią,</li>
+                        <li>antybiotykoterapia,</li>
+                        <li>zaburzenia krzepnięcia krwi.</li>
+                    </ul>
+                </div>
+                <div class="offer-page-list">
+                    <h3 class="h3">Dodatkowe informacje</h3>
+                    <div class="offer-page-list-text">
+                        <p>Przed zabiegiem nie należy stosować leków rozrzedzających krew.</p>
+                        </br>
+                        <p>Pełne efekty po podaniu toksyny można zaobserwować po 14 dniach, po tym czasie należy przyjść na wizytę kontrolna. 
+                        Toksyna botulinowa utrzymuje się od 3 do 6 miesięcy.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="bg-light-grey pos-rel">
+            <div class="glow top-100"></div>
+            <div class="container offer-page-container gridCols2">
+                <h2 class="offer-page-header"><span class="c-gold">kwas</span></br>hialuronowy</h2>
+                <div class="offer-page-text">
+                    <a class="offer-page-link" href="">Zobacz efekty</a>
+                </div>
+            </div>
+            <div class="offer-page-container-list gridCols3 no-border-bottom">
+                <div class="offer-page-list">
+                    <h3 class="h3">Wskazania</h3>
+                    <ul>
+                        <li>modelowanie/powiększanie ust,</li>
+                        <li>modelowanie owalu twarzy,</li>
+                        <li>wypełnienie głębokich zmarszczek,</li>
+                        <li>modelowanie brody,</li>
+                        <li>modelowanie żuchwy,</li>
+                        <li>modelowanie nosa,</li>
+                        <li>modelowanie policzków.</li>
+                    </ul>
+                </div>
+                <div class="offer-page-list">
+                    <h3 class="h3">Przeciwwskazania</h3>
+                    <ul>
+                        <li>ciąża,</li>
+                        <li>karmienie piersią,</li>
+                        <li>antybiotykoterapia,</li>
+                        <li>zaburzenia krzepnięcia krwi</li>
+                        <li>opryszczka.</li>
+                    </ul>
+                </div>
+                <div class="offer-page-list">
+                    <h3 class="h3">Dodatkowe informacje</h3>
+                    <div class="offer-page-list-text">
+                        <p>Przed zabiegiem nakładane jest miejscowe znieczulenie, ilość zużytego preparatu jest indywidualna. </p>
+                        </br>
+                        <p>Efekty utrzymują się od 6 do 9 miesięcy. Po zabiegu należy pić dużą ilość wody od 1,5-2l/dobę przez najbliższe 10/14 dni.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="offer-page-big-image-two">
+            <div class="offer-page-image">
+                <img src="<?php echo THEME_URL; ?>_dev/img/plant-on-table.jpg" alt="wazon z rośliną">
+            </div>
+        </section>
+        <section>
+            <div class="container offer-page-container gridCols2">
+                <h2 class="offer-page-header"><span class="c-gold">lipoliza</span></br>iniekcyjna</h2>
+                <div class="offer-page-text">
+                    <p>Lipoliza iniekcyjna to nieinwazyjne modelowanie sylwetki, polegające na wprowadzeniu za pomocą zastrzyków substancji powodującej likwidację komórek tłuszczowych. W rezultacie komórki ulegają rozpuszczeniu, uwolnione tłuszcze są rozkładane w procesie przemiany materii i wydalane z organizmu.</p>
+                    <a class="offer-page-link" href="">Zobacz efekty</a>
+                </div>
+            </div>
+            <div class="offer-page-container-list gridCols3">
+                <div class="offer-page-list">
+                    <h3 class="h3">Wskazania</h3>
+                    <ul>
+                        <li>podwójny podbródek,</li>
+                        <li>utrata jędrności skóry,</li>
+                        <li>potrzeba wymodelowania sylwetki.</li>
+                    </ul>
+                </div>
+                <div class="offer-page-list">
+                    <h3 class="h3">Przeciwwskazania</h3>
+                    <ul>
+                        <li>ciąża,</li>
+                        <li>karmienie piersią,</li>
+                        <li>antybiotykoterapia,</li>
+                        <li>zaburzenia krzepnięcia krwi</li>
+                        <li>ciężkie schorzenia wątroby i nerek</li>
+                        <li>infekcje skóry w miejscu wstrzyknięć</li>
+                        <li>alergia na preparat</li>
+                        <li>nadmierna otyłość.</li>
+                    </ul>
+                </div>
+                <div class="offer-page-list">
+                    <h3 class="h3">Dodatkowe informacje</h3>
+                    <div class="offer-page-list-text">
+                        <p>Najlepsze efekty uzyskujemy gdy pacjent prowadzi zdrowy tryb życia oraz zrównoważoną dietę.</p>
+                        </br>
+                        <p>Po zabiegu należy pamiętać o piciu dużej ilości wody 1,5l-2l/dobę przez najbliższe 10/14 dni.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container offer-page-container gridCols2">
+                <h2 class="offer-page-header"><span class="c-gold">osocze</span></h2>
+                <div class="offer-page-text">
+                    <p>Jest to substancja pozyskiwana z własnej krwi pacjenta, niewielka ilość krwi pobierana jest bezpośrednio przed zabiegiem, następnie podawana w skórę w formie mezoterapii.</p>
+                    </br>
+                    <p>Stymuluje produkcje kolagenu, opóźnia procesy starzenia, wpływa na procesy regeneracji skóry.
+                    Może być zastosowana w obrębie twarzy, szyi, dekoltu, grzbiety rak, owłosionej skory głowy.
+                    Ze względu na wykorzystanie autologicznego preparatu, praktycznie nie występują skutki uboczne.</p>
+                    <a class="offer-page-link" href="">Zobacz efekty</a>
+                </div>
+            </div>
+            <div class="offer-page-container-list gridCols3">
+                <div class="offer-page-list">
+                    <h3 class="h3">Wskazania</h3>
+                    <ul>
+                        <li>skóra pozbawiona blasku,</li>
+                        <li>zmęczona,</li>
+                        <li>widoczne zmarszczki,</li>
+                        <li>sucha skóra.</li>
+                    </ul>
+                </div>
+                <div class="offer-page-list">
+                    <h3 class="h3">Przeciwwskazania</h3>
+                    <ul>
+                        <li>ciąża,</li>
+                        <li>karmienie piersią,</li>
+                        <li>zaburzenia krzepnięcia krwi</li>
+                        <li>choroby nowotworowe</li>
+                        <li>przyjmowanie leków rozrzedzających krew.</li>
+                    </ul>
+                </div>
+                <div class="offer-page-list">
+                    <h3 class="h3">Dodatkowe informacje</h3>
+                    <div class="offer-page-list-text">
+                        <p>2 dni przed zabiegiem należy pić dużą ilość wody 1,5l-2l.</p>
+                        </br>
+                        <p>Po wykonaniu zabiegu nie stosować leków przeciwzapalnych oraz przeciwbólowych do 7 dni.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="bg-light-grey pos-rel">
+            <div class="glow top-50"></div>
+            <div class="container offer-page-container gridCols2">
+                <h2 class="offer-page-header"><span class="c-gold">fibryna</span></br>bogatopłyt&shy;kowa</h2>
+                <div class="offer-page-text">
+                    <p>Fibryna bogatopłytkowa to inaczej osocze, wzbogacone o leukocyty, płytki krwi i co najważniejsze komórki macierzyste, zawdzięcza się im działanie odmładzające.</p>
+                    </br>
+                    <p>Jest to substancja pozyskiwana z krwi pacjenta, niewielka ilość krwi pobierana jest bezpośrednio przed zabiegiem, następnie wirowana w probówce.</p>
+                    <a class="offer-page-link" href="">Zobacz efekty</a>
+                </div>
+            </div>
+            <div class="offer-page-container-list gridCols3">
+                <div class="offer-page-list">
+                    <h3 class="h3">Wskazania</h3>
+                    <ul>
+                        <li>nadmierne wypadanie włosów,</li>
+                        <li>skóra pozbawiona blasku,</li>
+                        <li>zmęczona,</li>
+                        <li>widoczne zmarszczki,</li>
+                        <li>sucha skóra.</li>
+                    </ul>
+                </div>
+                <div class="offer-page-list">
+                    <h3 class="h3">Przeciwwskazania</h3>
+                    <ul>
+                        <li>ciąża,</li>
+                        <li>karmienie piersią,</li>
+                        <li>zaburzenia krzepnięcia krwi</li>
+                        <li>choroby nowotworowe</li>
+                        <li>przyjmowanie leków rozrzedzających krew.</li>
+                    </ul>
+                </div>
+                <div class="offer-page-list">
+                    <h3 class="h3">Dodatkowe informacje</h3>
+                    <div class="offer-page-list-text">
+                        <p>2 dni przed zabiegiem należy pić dużą ilość wody 1,5l-2l.</p>
+                        </br>
+                        <p>Po wykonaniu zabiegu nie stosować leków przeciwzapalnych oraz przeciwbólowych do 7 dni.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+<?php get_footer(); ?>
