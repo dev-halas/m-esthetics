5d3eb431be8b2d5e5b1bd65ad16de96549073e69
devkryu <dev.halas@gmail.com>
2023-06-27 22:14:38 +0200

    logo height mobile fix

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
     <main>
         <?php 
             get_template_part('/components/home/about');
-            //get_template_part('/components/home/people');
+            get_template_part('/components/home/people');
             get_template_part('/components/home/offer');
         ?>
     </main>
