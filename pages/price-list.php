d04e76ebaabcc8d1e179efc13178fa37b732ce9d
kamilogrodowczyk <kamil.web.development@gmail.com>
2023-06-29 20:29:19 +0200

    Create price-list page

---
+++ pages/price-list.php
@@ -0,0 +1,220 @@
+<?php  
+
+    /* Template Name: Price list Page */
+
+    get_header();
+
+?>
+    <main>
+        <section id="price-list">
+            <div class="price-list-container">
+                <div>
+                    <h2 class="price-list-header c-gold">Medycyna estetyczna</h2>
+                </div>
+                <div class="price-list-text">
+                    <p>Osocze + 2 fibryny</p>
+                    <p>1100zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Osocze</p>
+                    <p>450zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Fibryna</p>
+                    <p>500zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Lipoza iniekcyjna</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>1 ampułka</p>
+                    <p>350zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>2 ampułki</p>
+                    <p>700zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>3 ampułki</p>
+                    <p>1000zł</p>
+                </div>
+                <div class="price-list-text second-level no-border-bottom">
+                    <p>4 ampułki</p>
+                    <p>1200zł</p>
+                </div>
+                <div class="price-list-text border-top">
+                    <p>Botox</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>3 okolice (bruzdy poprzeczne, lwia zmarszczka, kurze łapki)</p>
+                    <p>1000zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>bruzdy poprzeczne</p>
+                    <p>400-500zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>kurze łapki</p>
+                    <p>350-400zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>lwia zmarszczka</p>
+                    <p>350zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>broda marmurkowa</p>
+                    <p>300zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>zmarszczki królicze</p>
+                    <p>300zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>bruksizm</p>
+                    <p>??</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>uniesienie kącików ust</p>
+                    <p>300zł</p>
+                </div>
+                <div class="price-list-text second-level no-border-bottom">
+                    <p>otwarcie oka</p>
+                    <p>300zł</p>
+                </div>
+                <div class="price-list-text border-top">
+                    <p>Kwas hialuronowy 1ml</p>
+                    <p>800zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Radiess</p>
+                    <p>1800zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Mezoterapia igłowa</p>
+                    <p>400-800zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Hifu</p>
+                    <p>1100zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Hifu brzuch</p>
+                    <p>1400zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Hifu uda</p>
+                    <p>2000zł</p>
+                </div>
+                <div>
+                    <h2 class="price-list-header c-gold">Kosmetolo&shy;gia</h2>
+                </div>
+                <div class="price-list-text">
+                    <p>Dermapen</p>
+                    <p>600zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Dermapen z osoczem bogatopłytkowym</p>
+                    <p>900zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Retibooster </p>
+                    <p>450zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Smart DNA Therapy </p>
+                    <p>1300zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Melan Control Therapy </p>
+                    <p>1500zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Endolift fotona 4D</p>
+                    <p>1800zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Smooth eye</p>
+                    <p>450zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Rumień</p>
+                    <p>1100zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>twarz</p>
+                    <p>300zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>szyja</p>
+                    <p>300zł</p>
+                </div>
+                <div class="price-list-text second-level no-border-bottom">
+                    <p>dekolt</p>
+                    <p>200zł</p>
+                </div>
+                <div class="price-list-text border-top">
+                    <p>Laserowe leczenie chrapania</p>
+                    <p>500zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Laserowe usuwanie zmian skórnych</p>
+                    <p>od 150zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Naczynka</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>skrzydełka nosa</p>
+                    <p>250zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>nos</p>
+                    <p>300zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>cała twarz</p>
+                    <p>500-700zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>policzki</p>
+                    <p>400zł</p>
+                </div>
+                <div class="price-list-text second-level">
+                    <p>plamy rubinowe do 20 cm</p>
+                    <p>150-400zł</p>
+                </div>
+                <div class="price-list-text second-level no-border-bottom">
+                    <p>pojedyncze naczynko</p>
+                    <p>150zł</p>
+                </div>
+                <div class="price-list-text border-top no-border-bottom">
+                    <p>LipLase</p>
+                    <p>350zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Pakiet 4 - 1250 zł zamiast 1400 zł</p>
+                </div>
+                <div class="price-list-text no-border-bottom">
+                    <p>Laserowe stymulowanie wzrostu włosa</p>
+                    <p>500zł</p>
+                </div>
+                <div class="price-list-text">
+                    <p>Pakiet 4 - 1850 zł zamiast 2000 zł</p>
+                </div>
+                <div>
+                    <h2 class="price-list-header c-gold">Masaże</h2>
+                </div>
+                <div class="price-list-text">
+                    <p>Masaż lomi lomi</p>
+                    <p>120zł</p>
+                </div>
+            </div>
+        </section>
+        <section id="price-list-big-image">
+            <div class="price-list-image">
+                <img src="<?php echo THEME_URL; ?>_dev/img/plant-on-table.jpg" alt="wazon z rośliną">
+            </div>
+        </section>
+    </main>
+
+<?php get_footer(); ?>
